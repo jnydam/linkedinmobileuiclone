e27891628bf024b62abb2858ce2d24cb0e1953b7
John Nydam <jnydam@me.com>
2022-07-03 16:34:54 -0400

    Added more work to the button row container

--- src/components/MainLayoutComp/MainBodyComp/OuterUpperWhiteBubbleComp/ConnectionDescriptionComp/ConnectionDescriptionComp.js
+++ src/components/MainLayoutComp/MainBodyComp/OuterUpperWhiteBubbleComp/ConnectionDescriptionComp/ConnectionDescriptionComp.js
@@ -1,9 +1,50 @@
 import React from 'react';
 
+import styles from './ConnectionDescriptionComp.module.css';
+
 const ConnectionDescriptionComp = (props) => {
 
     return (<div className={styles.connectionDescriptionCompContainer}>
-        <span>This is the connection description comp</span>
+        <span style={{ marginLeft: '1.1rem'}}>181 connections</span>
+        <div className={styles.editButtonRowContainer}>
+            <div style={{
+                display: 'flex',
+                flex: 6,
+                backgroundColor: 'grey'
+            }}>
+                <button>Open to</button>
+            </div>
+            <div style={{
+                display: 'flex',
+                flex: 6,
+                backgroundColor: 'grey'
+            }}>
+                <button>Add section</button>
+            </div>
+            <div style={{
+                display: 'flex',
+                flex: 2,
+                backgroundColor: 'yellow',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <button style={{
+                    display: 'flex',
+                    outlineStyle: 'none',
+                    borderStyle: 'none',
+                    height: '2.5rem',
+                    width: '2.5rem',
+                    borderRadius: '1.25rem',
+                    overflow: 'hidden',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                    <div className={styles.smallDottedButtonStyle}></div>
+                    <div className={styles.smallDottedButtonStyle}></div>
+                    <div className={styles.smallDottedButtonStyle}></div>
+                </button>
+            </div>
+        </div>
     </div>)
 }
 
--- src/components/MainLayoutComp/MainBodyComp/OuterUpperWhiteBubbleComp/OuterUpperWhiteBubbleComp.js
+++ src/components/MainLayoutComp/MainBodyComp/OuterUpperWhiteBubbleComp/OuterUpperWhiteBubbleComp.js
@@ -3,11 +3,13 @@ import React from 'react';
 import styles from './OuterUpperWhiteBubbleComp.module.css';
 
 import HeaderProfileInfoComp from './HeaderProfileInfoComp/HeaderProfileInfoComp';
+import ConnectionDescriptionComp from './ConnectionDescriptionComp/ConnectionDescriptionComp';
 
 const OuterUpperWhiteBubbleComp = (props) => {
 
     return (<div className={styles.outerUpperWhiteBubbleCompContainer}>
         <HeaderProfileInfoComp></HeaderProfileInfoComp>
+        <ConnectionDescriptionComp></ConnectionDescriptionComp>
     </div>)
 }
 
