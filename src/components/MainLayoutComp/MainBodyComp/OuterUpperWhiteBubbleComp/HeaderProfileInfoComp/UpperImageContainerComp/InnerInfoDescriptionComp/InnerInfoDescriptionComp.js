a0532d60d9a49ea51316aea34f9ea7b103ae603f
John Nydam <jnydam@me.com>
2022-07-03 15:59:05 -0400

    Removed color scaffolding from header component

---
+++ src/components/MainLayoutComp/MainBodyComp/OuterUpperWhiteBubbleComp/HeaderProfileInfoComp/UpperImageContainerComp/InnerInfoDescriptionComp/InnerInfoDescriptionComp.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import styles from './InnerInfoDescriptionComp.module.css';
+
+const InnerInfoDescriptionComp = (props) => {
+
+    return (<div className={styles.innerInfoDescriptionCompContainer}>
+        <span style={{
+            fontWeight: '600',
+            fontSize: '1.4rem',
+            marginLeft: '1.1rem'
+        }}>John Nydam</span>
+        <span style={{
+            marginLeft: '1.1rem'
+        }}>Software Engineer at Fidelity Investments</span>
+        <div className={styles.workEducationRowContainer}>
+            <span style={{
+                marginLeft: '1.1rem'
+            }}>Fidelity Investments</span>
+            <div style={{ 
+                backgroundColor: 'black',
+                height: '0.2rem', 
+                marginLeft: '0.3rem',
+                marginRight: '0.3rem',
+                width: '0.2rem', 
+                borderRadius: '0.1rem' }}>
+            </div>
+            <span>Brandeis University</span>
+        </div>
+        <span style={{
+            marginLeft: '1.1rem',
+            color: '#d3d3d3'
+        }}>Merrimack, New Hampshire, United States</span>
+    </div>)
+}
+
+export default InnerInfoDescriptionComp;
