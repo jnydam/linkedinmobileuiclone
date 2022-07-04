8d56e4b9111d5356384cd71fcebf4d3f0e2f987a
John Nydam <jnydam@me.com>
2022-07-03 20:18:58 -0400

    Added window dimension error handling

--- src/components/MainLayoutComp/MainLayoutComp.js
+++ src/components/MainLayoutComp/MainLayoutComp.js
@@ -8,11 +8,39 @@ import UpperNavBarComp from './UpperNavBarComp/UpperNavBarComp';
 
 const MainLayoutComp = (props) => {
 
-    return (<div className={styles.mainLayoutCompContainer}>
+    return (
+    <React.Fragment>
+    <div className={styles.mainLayoutCompContainer}>
         <UpperNavBarComp></UpperNavBarComp>
         <MainBodyComp></MainBodyComp>
         <LowerTabComp></LowerTabComp>
-    </div>)
+    </div>
+    <div className={styles.mainLayoutCompContainerLarge}>
+         <div style={{
+             width: '50%'
+         }}>
+             <span style={{display: 'flex'}}>
+                Looks like your screen size is a little too big
+                for this project.
+             </span>
+
+             <span style={{
+                 display: 'flex',
+                 marginTop: '1.5rem'
+             }}>
+             Since this user interface is designed for mobile,
+             please resize the screen roughly at or below 700 px width.
+             </span>
+             <span style={{
+                 display: 'flex',
+                 marginTop: '1.5rem'
+             }}>
+                You could achieve this by either resizing your browser window or view this site on a mobile device.
+             </span>
+
+         </div>
+    </div>
+    </React.Fragment>)
 }
 
 export default MainLayoutComp;
